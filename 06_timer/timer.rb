ccbd99c8178fe6183b5b19c93e49a55795fd8f96
laura ballo <laura.ballo1993@gmail.com>
2018-01-16 23:58:21 +0100

    exercice 006

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,13 @@
 class Timer
+	attr_accessor :seconds
   #write your code here
+  def initialize(time_string)
+  	@seconds = seconds
+  	@timer = timer
+  	time_string = t.strftime("%H:%M:%S")
+  end
 end
+
+Timer.new(time_string)
+Timer.padded((1))
+
