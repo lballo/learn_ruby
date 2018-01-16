9e09c8e780245cb23e9aca0d7902349bf16c0a8d
laura ballo <laura.ballo1993@gmail.com>
2018-01-16 23:36:55 +0100

    exos 005

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,13 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+
+	def initialize(title)
+	@title = title
+	@title = string.capitalize
+	end
 end
+
+string = Book.new
+string.title = "#{string}"
+#erreur dans le code mais où?!
