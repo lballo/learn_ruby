21b5c5501ad9bedd7713caa8a3ac690715a52d7a
laura ballo <laura.ballo1993@gmail.com>
2018-01-17 00:06:14 +0100

    modification Timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,13 +1,27 @@
+# class Timer
+# 	attr_accessor :seconds
+#   #write your code here
+#   def initialize(time_string)
+#   	@seconds = seconds
+#   	@timer = timer
+#   	time_string = t.strftime("%H:%M:%S")
+#   end
+# end
+
+# Timer.new(time_string)
+# Timer.padded((1))
+
 class Timer
 	attr_accessor :seconds
-  #write your code here
-  def initialize(time_string)
-  	@seconds = seconds
-  	@timer = timer
-  	time_string = t.strftime("%H:%M:%S")
-  end
+
+	def initialize
+		@seconds = 0
+	end
 end
 
-Timer.new(time_string)
-Timer.padded((1))
-
+def time_string
+	hr = (@seconds / 3600).to_s
+	min = ((@seconds / 60) % 60).to_s
+ 	sec = (@seconds % 60).to_s
+ 	output = "#{hr.rjust(2, "0")}:#{min.rjust(2, "0")}:#{sec.rjust(2, "0")}"
+ end
