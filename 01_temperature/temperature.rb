3a8c59badb69db0fe1c15ca507530b8dbb2dc6b3
laura ballo <laura.ballo1993@gmail.com>
2018-01-16 16:41:07 +0100

    Exercices 01 et 02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,12 @@
 #write your code here
+def ftoc(fahrenheit)
+	celsius = 5.0/9.0*(fahrenheit - 32)
+end
+
+print ftoc(32)
+
+def ctof(celsius)
+	fahrenheit = 9.0/5.0*celsius + 32
+end
+
+print ctof(0)
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,18 @@
 #write your code here
+def add(var1, var2)
+	result = var2 + var1
+	return result
+end
+
+def subtract(a,b)
+	result1 = a - b
+	return result1
+end
+
+def sum(array)
+	result2 = 0
+array.each do  |i|
+	result2 = result2 + i
+	end
+	return result2
+end
