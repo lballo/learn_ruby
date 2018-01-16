9e5220ac15005830e1bfdbd7642162252d7c6f0d
laura ballo <laura.ballo1993@gmail.com>
2018-01-16 19:35:59 +0100

    ajout exo 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
 #write your code here
+def echo(string)
+	return string
+end
+
+def shout(string2)
+	return string2.upcase
+end
+
+def repeat(string3, n = 2)
+	return ([string3] * n).join(" ")
+end
+
+print repeat("hello", 3)
+
+def start_of_word(string4, n = 0)
+	string4.slice(0, n)
+end
+
+def first_word(string5)
+	string5.split.first
+end
+
+def titleize(phrase)
+	phrase.split('')
+	exceptions =['and', 'the']
+
+	if phrase.include?exceptions{ 
+		phrase_finale
+	}
+	else phrase.capitalize('')
+		return phrase_finale
+	end
+end
+phrase = "There is"
